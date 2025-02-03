8545be7c29e7acefb763e7682d40be5b95d79760
Angel <al03111874@tecmilenio.mx>
2025-02-02 18:01:38 -0600

    DEV--Conexion node

--- app/Index.js
+++ app/Index.js
@@ -1,7 +1,17 @@
 import  express  from "express";
+
+//Arreglo del _dirname
+import path from 'path';
+import { fileURLToPath } from "url";
+const __dirname=path.dirname(fileURLToPath(import.meta.url));
+
 //server
 const app = express();
 app.set('port',4000);
-app.listen(app.get('port'));
-console.log("serveidor corriendo en puerto",app.get('port'));
-
+app.listen(app.get('port'),()=>{
+    console.log("serveidor corriendo en puerto",app.get('port'));
+});
+//configuracion
+app.use(express.static(path.join(__dirname,"app")))
+//Rutas
+app.get("/",(req,res)=>res.sendFile(__dirname +"/index.html"))
