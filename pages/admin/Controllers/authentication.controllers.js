b8fbcfc5ded4969de6da5adf346e3cf7889a6a10
Angel <al03111874@tecmilenio.mx>
2025-02-03 06:44:26 -0600

    Dev--Crear Validaciones para evitar duplicados de registro

--- pages/admin/Controllers/authentication.controllers.js
+++ pages/admin/Controllers/authentication.controllers.js
@@ -1,11 +1,29 @@
+const usuarios=[{
+    user:"a",
+    email:"a@a.com",
+    password:"a"
+
+}]
+
+import { error } from "console";
+
 function Login(req,res){
 
 }
 function register(req,res){
     console.log(req.body)
+    const user=req.body.user;
+    const email=req.body.email;
+    const password=req.body.password;
 
-
+    if(!user || !email || !password)
+        res.status(400).send({status:"Error",message:"LOS CAMPOS ESTAN INCOMPLETOS"})
+    const usuarioARevisar = usuarios.find(usuario =>usuario.user === user);
+    if(usuarioARevisar){
+        res.status(400).send({status:"Error",message:"Este usuario ya existe"})
+    }
 }
+
 
 export const methods = {
     Login: (req, res) => {
