adb65626caa1c35e092517f4c265edf6ba1e9366
Angel <al03111874@tecmilenio.mx>
2025-02-03 01:22:36 -0600

    DEV-Creacion del inicio de sesion

--- js/Index.js
+++ js/Index.js
@@ -11,8 +11,10 @@ app.set('port',4000);
 app.listen(app.get('port'),()=>{
     console.log("serveidor corriendo en puerto",app.get('port'));
 });
+
 //configuracion
 app.use(express.static(path.join(__dirname,"app")))
 //Rutas
 app.get("/",(req,res)=>res.sendFile(__dirname +"/index.html"))
-app.get("Login",(req,res)=>res.sendFile(__dirname +"/Login.html"))
+app.get("/Login",(req,res)=>res.sendFile(__dirname +"/pages/Login.html"))
+app.get("/admin",(req,res)=>res.sendFile(__dirname +"/admin/admin.html"))
---
+++ js/Register.js
@@ -0,0 +1,5 @@
+document.getElementById("register-form").addEventListener("submit",async(e) => {
+    e.preventDefault(); 
+    console.log(e.target.children['user'].value); 
+    const res = await tech ("")
+});
